3cbbad9520894e7a729cde652e43e6dad6578b83
guicastrorl <57015354+guicastrorl@users.noreply.github.com>
2020-06-08 00:26:12 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 //Conexão com BD MYSQL
-const sequelize = new Sequelize('historico', 'root', 'senha', {
+const sequelize = new Sequelize('historico', 'root', 'Qwerty123', {
   host: 'localhost',
   dialect: 'mysql'
 });
